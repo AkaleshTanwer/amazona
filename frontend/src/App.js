b8fdcfca8cce3b5a72b2842494faccf6bcb0dc39
atanwer <akaleshtanwer5014@gmail.com>
2022-04-28 15:06:46 +0530

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <>
+      <header>
+
+        <a
+          href="/"
+        >Amazona
+        </a>
+      </header>
+      <main>List Items</main>
+
+    </>
+  );
+}
+
+export default App;
